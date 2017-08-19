64a3f80d01ef9a37dfd764e9f21ed9f469a3cbd6
u4bi <u4bi@u4bi.com>
2017-08-19 11:41:33 +0900

    board graphql query render - board - boards - addBoard - deleteBoard - editBoard
    data
    
    - assets/board.json
    
    run script
    
    - npm run board-dev:server
    - npm run board-json:server

---
+++ board/app.js
@@ -0,0 +1,183 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const axios = require('axios');
+const { 
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull
+} = require('graphql');
+
+
+
+
+const BoardsType = new GraphQLObjectType({
+    name : 'Boards',
+    fields : () => ({
+        id    : { type : GraphQLString },
+        writer : { type : GraphQLString },
+        title : { type : GraphQLString },                
+        content  : { type : GraphQLString }
+    }) 
+});
+
+
+const RootQuery = new GraphQLObjectType({
+    name : 'RootQueryType',
+    fields: {
+        board :{
+            type : BoardsType,
+            args : {
+                id : { type : GraphQLString }
+            },
+            resolve(parentValue, args){
+                
+            return axios.get('http://localhost:3000/boards/' + args.id)
+                .then(res => res.data);
+            }
+        },
+        //
+        boards : {
+            type : new GraphQLList(BoardsType),
+            resolve(parentValue, args){
+                
+                return axios.get('http://localhost:3000/boards')
+                    .then(res => res.data);
+
+            }
+        }
+    } 
+});
+
+
+// mutations
+
+const mutation = new GraphQLObjectType({
+    name : 'Mutation',
+    fields : {
+        addBoard : {
+            type : BoardsType,
+            args : {
+
+                writer : { type : new GraphQLNonNull(GraphQLString) },
+                title : { type : new GraphQLNonNull(GraphQLString) },                               
+                content : { type : new GraphQLNonNull(GraphQLString) }
+            },
+            resolve(parentValue, args){
+                
+                return axios.post('http://localhost:3000/boards', {
+                    writer : args.writer,
+                    title : args.title,
+                    content : args.content
+
+                })
+                .then(res => res.data);
+            }
+        },
+        //
+        deleteBoard : {
+            type : BoardsType,
+            args : {
+                id : { type : new GraphQLNonNull(GraphQLString)}
+            },
+            resolve(parentValue, args){
+                
+                return axios.delete('http://localhost:3000/boards/' + args.id)
+                .then(res => res.data);
+
+            }
+        },
+        //
+        editBoard : {
+            type : BoardsType,
+            args : {
+                id : { type : new GraphQLNonNull(GraphQLString)},
+                writer : { type : GraphQLString },
+                title : { type : GraphQLString },
+                content : { type : GraphQLString }
+            },
+            resolve(parentValue, args){
+                
+                return axios.patch('http://localhost:3000/boards/' + args.id, args)
+                .then(res => res.data);
+            }
+        },        
+    }
+});
+
+
+
+const schema = new GraphQLSchema({
+    query : RootQuery,
+    mutation
+});
+
+const app = express();
+
+
+app.use('/graphql', expressGraphQL({
+    schema   : schema,
+    graphiql : true
+}));
+
+app.listen(4000, ()=> {
+    console.log('on port 4000..');
+});
+
+
+
+/*
+
+        {
+            board(id : "1"){
+                id,
+                writer,		
+                title,
+                content
+            }
+        }
+
+
+
+        {
+            boards{
+                id,
+                writer,		
+                title,
+                content
+            }
+        }
+
+
+
+        mutation{
+            addBoard( writer:"aaa", title : "aaa", content : "aaa" ){
+                id,
+                writer,		
+                title,
+                content
+            }
+        }
+        
+
+
+        mutation{
+            editBoard(id:"1", title : "edit title"){
+                id,
+                writer,
+                title,
+                content
+            }
+        }
+        
+
+
+        mutation{
+            deleteBoard(id:"1"){
+                id
+            }
+        }
+          
+*/
